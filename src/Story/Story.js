4c3534816862fdc070770ae2331e6e10e62244d9
Luther Karper <luther.karper@live.com>
2020-07-08 16:10:48 -0400

    Implement api call in Story component

--- src/Story/Story.js
+++ src/Story/Story.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Story = (props) => {
+
+    const { chapt } = props.match.params;
+
+    const [dialogue, setDialogue] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/api/dialogue/${chapt}`)
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw new Error (res.statusText);
+            })
+            .then(data => {
+                console.log(data);
+                setDialogue(data);
+            });
+    }, [chapt]);
+
     return (
         <div>
 
