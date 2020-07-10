181be31be9486b31385151ae05a87004953ed567
Luther Karper <luther.karper@live.com>
2020-07-10 16:13:36 -0400

    Add Exercise Component

--- src/Exercise/Exercise.js
+++ src/Exercise/Exercise.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Learn from '../Learn/Learn';
+import Do from '../Do/Do';
 
 const Exercise = (props) => {
     return (
         <div>
+            <Route 
+                path="/game/exercise/:chapt/learn"
+                component={Learn}
+            />
+            <Route 
+                path="/game/exercise/:chapt/do"
+                component={Do}
+            />
 
         </div>
     );
