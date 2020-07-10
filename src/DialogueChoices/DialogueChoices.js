04b8d8f524b2e1183f52465ab90216ea91de6c45
Luther Karper <luther.karper@live.com>
2020-07-10 14:23:47 -0400

    Refactor code to improve readability

--- src/DialogueChoices/DialogueChoices.js
+++ src/DialogueChoices/DialogueChoices.js
@@ -4,7 +4,8 @@ import DialogueResponse from '../DialogueResponse/DialogueResponse';
 const DialogueChoices = (props) => {
     const {dialogue, page, setResponse } = props.data;
     const { choices } = dialogue[page - 1];
-    const choicesHTML = choices.split('|').map((choice, i) => <button key={i} onClick={() => setResponse(<DialogueResponse data={{dialogue, page, choiceIndex: i}}/>)}>{choice}</button>)
+    const choicesHTML = choices.split('|').map((choice, i) => 
+        <button key={i} onClick={() => setResponse(<DialogueResponse data={{dialogue, page, choiceIndex: i}}/>)}>{choice}</button>);
     return (
         <div>
             {choicesHTML}
