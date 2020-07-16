c3846a13612c26ea3c1c28d73467e770f64287a6
Luther Karper <luther.karper@live.com>
2020-07-16 15:09:44 -0400

    Add progress report to Dashboard

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NotesList from '../NotesList/NotesList';
+import ProgressReport from '../ProgressReport/ProgressReport';
 
 const Dashboard = () => {
 
     return (
         <div>
+            <ProgressReport />
             <NotesList />
-
         </div>
     );
 }
