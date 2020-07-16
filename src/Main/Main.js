7b7a04101264a74571ca66b07795d2c98c5d7723
Luther Karper <luther.karper@live.com>
2020-07-15 20:44:17 -0400

    Add Dashboard route to Main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import PublicOnlyRoute from '../utils/PublicOnlyRoute';
+import PrivateOnlyRoute from '../utils/PrivateOnlyRoute';
 import Landing from '../Landing/Landing';
 import Game from '../Game/Game';
 import Register from '../Register/Register';
 import LoginPage from '../LoginPage/LoginPage';
+import Dashboard from '../Dashboard/Dashboard';
+import './Main.css';
 
 const Main = (props) => {
 
@@ -19,13 +23,17 @@ const Main = (props) => {
                 path={["/game/story/:chapt", "/game/exercises/:chapt"]}
                 component={Game}
             />
-            <Route
+            <PublicOnlyRoute
                 path="/register"
                 component={Register}
             />
-            <Route
+            <PublicOnlyRoute
                 path="/login"
                 render={rProps => <LoginPage {...rProps} forceUpdate={forceUpdate} />} 
+            />
+            <PrivateOnlyRoute 
+                path="/dashboard"
+                component={Dashboard}
             />
         </main>
     );
