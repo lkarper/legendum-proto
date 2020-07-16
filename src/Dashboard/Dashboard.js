f6ba78e0b7ccdbf5db0ee212a4ec29d92d37ff95
Luther Karper <luther.karper@live.com>
2020-07-15 20:42:40 -0400

    Add Dashboard component for logged in users

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import NotesList from '../NotesList/NotesList';
+
+const Dashboard = () => {
+
+    return (
+        <div>
+            <NotesList />
+
+        </div>
+    );
+}
+
+export default Dashboard;
