e16db54bf221ab4db1b35c9cecbea5bc64125d4b
Luther Karper <luther.karper@live.com>
2020-07-16 15:10:37 -0400

    Update landing page
    Landing page now displays different links to users that are logged in
    vs. non-registered users.

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TokenService from '../services/token-service';
+import NextStory from '../NextStory/NextStory';
 import './Landing.css';
 
 const Landing = (props) => {
     return (
         <div className="Landing__container">
-            <Link
-                to="/register"
-            >Register</Link>
-            <Link
-                to="/game/story/1"
-            >Play without Registering</Link>
-            <Link
-                to="/login"
-            >Login</Link>
+            {!TokenService.hasAuthToken() ? 
+                <>
+                    <Link
+                        to="/register"
+                    >Register</Link>
+                    <Link
+                        to="/game/story/1"
+                    >Play without Registering</Link>
+                    <Link
+                        to="/login"
+                    >Login</Link>
+                </>
+                : 
+                <>
+                    <Link
+                        to='/dashboard'
+                    >Go to Dashboard</Link>
+                    <NextStory />
+                </>    
+            }
         </div>
     )
 }
---
+++ src/NextStory/NextStory.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'
+import UserContext from '../contexts/UserContext';
+
+const NextStory = (props) => {
+
+    const context = useContext(UserContext);
+
+    const exercises = context.exercises.sort((a, b) => a.id - b.id);
+
+    const lastChapter = context.exercises.length ? context.exercises.sort((a, b) => b.id - a.id)[0].id : undefined;
+
+    let nextChapter;
+
+    exercises.forEach(exercise => {
+        if (!nextChapter) {
+            if (context.progress.filter(p => p.exercise_id === exercise.id).length === 0) {
+                nextChapter = exercise.id;
+            }
+        }
+    });
+
+    if (nextChapter) {
+        return <Link to={`/game/story/${nextChapter}`}>Pick up where you left off (chapter {nextChapter})</Link>;
+    } else if (context.progress.length === 0) {
+        return <Link to={`/game/story/1`}>Start at Chapter 1</Link>;
+    }
+    
+    return <Link to={`/game/story/${lastChapter}`}>Visit the last chapter again</Link>;
+}
+
+export default NextStory;
