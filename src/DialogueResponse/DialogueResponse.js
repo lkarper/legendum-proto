64fdab1f5213e3e97a0b7fc02a1f34eb4263900f
Luther Karper <luther.karper@live.com>
2020-07-21 17:41:54 -0400

    Add class

--- src/DialogueResponse/DialogueResponse.js
+++ src/DialogueResponse/DialogueResponse.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './DialogueResponse.css';
 
 const DialogueResponse = (props) => {
     const { dialogue, choiceIndex, page } = props.data;
     const { responses_to_choices } = dialogue[page - 1];
     const responsesArray = responses_to_choices.split("|");
     return (
-        <p>{responsesArray[choiceIndex]}</p>
+        <p className='DialogueResponse__text'>{responsesArray[choiceIndex]}</p>
     );
 }
 
