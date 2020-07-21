9a1aeae6b86ca006ed78ee91f4e89c9cd0d744e3
Luther Karper <luther.karper@live.com>
2020-07-21 17:51:07 -0400

    Update SaveHint component
    Component was reorganized by removing an unnecessary div, altering the
    size of the textarea element, and adding a class name.

--- src/SaveHint/SaveHint.js
+++ src/SaveHint/SaveHint.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ValidateCustomNote from '../ValidateCustomNote/ValidateCustomNote';
+import './SaveHint.css';
 
 const SaveHint = (props) => {
 
@@ -9,30 +10,29 @@ const SaveHint = (props) => {
     const [customNoteError, setCustomNoteError] = useState('');
 
     return (
-        <div>
-            <form onSubmit={(e) => onSubmitHint(e, customNote)}>
-                <textarea
-                    id="custom-note"
-                    name="custom-note"
-                    aria-label="Add a custom note here, if you'd like."
-                    placeholder="Add a custom note here, if you'd like."
-                    rows="4"
-                    cols="50"
-                    value={customNote}
-                    onChange={(e) => setCustomNote(e.target.value)}
-                ></textarea>
-                <button 
-                    type="submit"
-                    disabled={!!customNoteError}
-                >Save Hint</button>
-                <ValidateCustomNote 
-                    customNote={customNote}
-                    customNoteError={customNoteError}
-                    setCustomNoteError={setCustomNoteError}
-                />
-            </form>
-        </div>
-    )
+        <form onSubmit={(e) => onSubmitHint(e, customNote)}>
+            <textarea
+                id="custom-note"
+                name="custom-note"
+                aria-label="Add a custom note here, if you'd like."
+                placeholder="Add a custom note here, if you'd like."
+                rows="4"
+                cols="35"
+                value={customNote}
+                onChange={(e) => setCustomNote(e.target.value)}
+            ></textarea>
+            <button 
+                className='SaveHint__submit'
+                type="submit"
+                disabled={!!customNoteError}
+            >Save Hint</button>
+            <ValidateCustomNote 
+                customNote={customNote}
+                customNoteError={customNoteError}
+                setCustomNoteError={setCustomNoteError}
+            />
+        </form>
+    );
 }
 
 export default SaveHint;
