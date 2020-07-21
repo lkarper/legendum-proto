d8772f56dd093d4045d3e3b5b2f5c79daa4215c0
Luther Karper <luther.karper@live.com>
2020-07-21 17:46:10 -0400

    Refactor to improve readability

--- src/Learn/Learn.js
+++ src/Learn/Learn.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import ExercisesService from '../services/exercises-service';
 import LearnPage from '../LearnPage/LearnPage';
+import './Learn.css';
 
 const Learn = (props) => {
     const { chapt } = props.match.params;
@@ -21,23 +21,22 @@ const Learn = (props) => {
     }, [chapt]);
 
     return (
-        <div>
-            {pages.length ? < h2>{pages[0].exercise_title}</h2> : <p>Loading...</p>}
-            {pages.length ? 
-                <LearnPage
-                    data={{
-                        pages,
-                        page,
-                        setPage
-                    }}
-                />
-                : ''
-            }
-            {page === pages.length ?
-                <Link to={`/game/exercises/${chapt}/do`}>Practice What You've Learned</Link>
-                : ''
-            }
-        </div>
+        <>
+            {pages.length 
+                ? 
+                    <div className='Learn__container'>
+                        <h2 className='Learn__h2'>{pages[0].exercise_title}</h2> 
+                        <LearnPage
+                            data={{
+                                pages,
+                                page,
+                                chapt,
+                                setPage,
+                            }}
+                        />
+                    </div>
+                : <p>Loading...</p>}
+        </>
     );
 }
 
