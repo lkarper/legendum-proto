c18bc1afbcf69f9b0a720c461026c8217e4f661c
Luther Karper <luther.karper@live.com>
2020-07-22 12:17:10 -0400

    Add NotesWidget component

---
+++ src/NotesWidget/NotesWidget.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import TokenService from '../services/token-service';
+import NotesList from '../NotesList/NotesList';
+import './NotesWidget.css';
+
+const NotesWidget = (props) => {
+
+    const [showList, toggleShowList] = useState(false);
+
+    const notesListContainerClassSuffix = showList ? 'widget-list on' : 'widget-list off';
+
+    if (TokenService.hasAuthToken()) {
+        return (
+            <>
+                <button
+                    className='NotesWidget__toggle-button'
+                    onClick={() => toggleShowList(!showList)}
+                >
+                    My notes
+                </button>
+                <div className={`NotesWidget__outer-container-${notesListContainerClassSuffix}`}>
+                    <button
+                        className='NotesWidget__outer-container-close-button'
+                        onClick={() => toggleShowList(false)}
+                    >
+                        &#10006;
+                    </button>
+                    <NotesList suffix={'widget-list'} />
+                </div>
+            </>
+        );
+    }
+    return <></>;
+}
+
+export default NotesWidget;
