6436f5f798e4ddf37fd7dd09b4315983beaa411f
Luther Karper <luther.karper@live.com>
2020-10-08 16:45:43 -0400

    Update DialogueChoices
    Refactor code to improve readability.

--- src/DialogueChoices/DialogueChoices.js
+++ src/DialogueChoices/DialogueChoices.js
@@ -3,16 +3,34 @@ import DialogueResponse from '../DialogueResponse/DialogueResponse';
 import './DialogueChoices.css';
 
 const DialogueChoices = (props) => {
-    const {dialogue, page, setResponse } = props.data;
+    const {
+        dialogue, 
+        page, 
+        setResponse 
+    } = props.data;
+
     const { choices } = dialogue[page - 1];
-    const choicesHTML = choices.split('|').map((choice, i) => 
-        <button 
-            className='DialogueChoices_choice-button button'
-            key={i} 
-            onClick={() => setResponse(<DialogueResponse data={{dialogue, page, choiceIndex: i}}/>)}
-        >
-            {choice}
-        </button>);
+
+    const choicesHTML = choices
+        .split('|')
+        .map((choice, i) => 
+            <button 
+                className='DialogueChoices_choice-button button'
+                key={i} 
+                onClick={() => setResponse(
+                    <DialogueResponse 
+                        data={{
+                            dialogue, 
+                            page, 
+                            choiceIndex: i
+                        }}
+                    />
+                )}
+            >
+                {choice}
+            </button>
+        );
+
     return (
         <div className='DialogueChoices__container'>
             {choicesHTML}
