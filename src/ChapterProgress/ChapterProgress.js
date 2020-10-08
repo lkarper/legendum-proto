0bf14eaa72edd42bf5c142125af2734dee264d9c
Luther Karper <luther.karper@live.com>
2020-10-08 15:49:50 -0400

    Update ChapterProgress component
    Updated code to use updated API.

--- src/ChapterProgress/ChapterProgress.js
+++ src/ChapterProgress/ChapterProgress.js
@@ -1,36 +1,41 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import UserContext from '../contexts/UserContext';
 import './ChapterProgress.css';
 
 const ChapterProgress = (props) => {
-
-    const context = useContext(UserContext);
     const { exercise } = props;
 
-    const timesCompleted = context.progress.filter(p => p.exercise_id === exercise.id).length;
+    const context = useContext(UserContext);
+    const timesCompleted = context.progress.filter(p => p.chapter_number === exercise.chapter_number).length;
 
     return (
         <li>
             <h4>{exercise.exercise_title}{' '}{exercise.exercise_translation}</h4>
             <p>You've completed this chapter {timesCompleted === 1 ? '1 time' : `${timesCompleted} times`}</p>
-            {timesCompleted ? 
+            {timesCompleted !== 0 && 
                 <div> 
                     <h5>You completed this exercise on: </h5>
                     <ul>
                         {context.progress
-                            .filter(p => p.exercise_id === exercise.id)
-                            .map(p => <li key={p.id}>{new Date(p.date_completed).toString()}</li>)
+                            .filter(p => p.chapter_number === exercise.chapter_number)
+                            .map(p => 
+                                <li 
+                                    key={p.id}
+                                >
+                                    {moment(p.date_completed).format('MMM. Do YYYY, h:mm a')}
+                                </li>
+                            )
                         }
                     </ul>
                 </div>
-                : ''
             }
             <div className='ChapterProgress__link-container'>
                 <Link
                     className='ChapterProgress__link' 
-                    to={`/game/story/${exercise.id}`}
+                    to={`/game/story/${exercise.chapter_number}`}
                 >
                     Pick up with this chapter's story
                 </Link>
@@ -40,7 +45,7 @@ const ChapterProgress = (props) => {
                 />
                 <Link 
                     className='ChapterProgress__link'
-                    to={`/game/exercises/${exercise.id}/learn`}
+                    to={`/game/exercises/${exercise.chapter_number}/learn`}
                 >
                     Learn this Exercise
                 </Link>
@@ -50,7 +55,7 @@ const ChapterProgress = (props) => {
                 />
                 <Link 
                     className='ChapterProgress__link'
-                    to={`/game/exercises/${exercise.id}/do`}
+                    to={`/game/exercises/${exercise.chapter_number}/do`}
                 >
                     Practice this Exercise
                 </Link>
