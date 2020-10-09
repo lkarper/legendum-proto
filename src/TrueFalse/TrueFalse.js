d27eaca532c055a56c39fc4e99cee4a9366e528f
Luther Karper <luther.karper@live.com>
2020-10-08 21:59:42 -0400

    Refactor code to improve readability

--- src/TrueFalse/TrueFalse.js
+++ src/TrueFalse/TrueFalse.js
@@ -4,14 +4,20 @@ import QuestionLegend from '../QuestionLegend/QuestionLegend';
 import './TrueFalse.css';
 
 const TrueFalse = (props) => {
+    const { 
+        page, 
+        savedUserInput, 
+        checkAnswer,
+    } = props;
 
     const [userChoice, setChoice] = useState();
 
-    const { page, savedUserInput, checkAnswer } = props;
-
     return (
         <div className='TrueFalse__container'>
-            <QuestionDialogue page={page} savedUserInput={savedUserInput} />
+            <QuestionDialogue 
+                page={page} 
+                savedUserInput={savedUserInput} 
+            />
             <form
                 onSubmit={(e) => {
                     checkAnswer(e, userChoice);
@@ -19,41 +25,47 @@ const TrueFalse = (props) => {
                 }}
             >
                 <fieldset>
-                    <QuestionLegend page={page} savedUserInput={savedUserInput} />
-                    <div>
+                    <QuestionLegend 
+                        page={page} 
+                        savedUserInput={savedUserInput} 
+                    />
+                    <div
+                        className='TrueFalse__input-div'
+                    >
                         <input 
-                            type="radio"
-                            id="true"
-                            name="user-response"
-                            value="True/Vērum"
+                            type='radio'
+                            id='true'
+                            name='user-response'
+                            value='True/Vērum'
                             checked={userChoice === 'True/Vērum'}
                             onChange={(e) => setChoice(e.target.value)}
                             required
                         />
-                        <label htmlFor="true">True/Vērum</label>
+                        <label htmlFor='true'>True/Vērum</label>
                     </div>
-                    <div>
+                    <div
+                        className='TrueFalse__input-div'
+                    >
                         <input 
-                            type="radio"
-                            id="false"
-                            name="user-response"
-                            value="False/Falsum"
+                            type='radio'
+                            id='false'
+                            name='user-response'
+                            value='False/Falsum'
                             checked={userChoice === 'False/Falsum'}
                             onChange={(e) => setChoice(e.target.value)}
                             required
                         />
-                        <label htmlFor="false">False/Falsum</label>
+                        <label htmlFor='false'>False/Falsum</label>
                     </div>
                 </fieldset>
                 <button
                     className='TrueFalse__submit button'
-                    type="submit"
+                    type='submit'
                     disabled={!userChoice}
                 >
                     Submit
                 </button>
             </form>
-
         </div>
     );
 }
