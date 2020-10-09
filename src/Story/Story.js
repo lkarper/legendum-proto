cf101aad54695d3886d54c6c9103bafde46a2c82
Luther Karper <luther.karper@live.com>
2020-10-08 21:59:04 -0400

    Update Story component
    Added code to handle API errors.

--- src/Story/Story.js
+++ src/Story/Story.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import DialoguePage from '../DialoguePage/DialoguePage';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import config from '../config';
 import './Story.css';
 
 const Story = (props) => {
-
     const { chapt } = props.match.params;
 
     const [storyTitle, setStoryTitle] = useState('');
     const [dialogue, setDialogue] = useState([]);
     const [page, setPage] = useState(1);
-    const [response, setResponse] = useState();    
+    const [response, setResponse] = useState();
+    const [showLoading, setShowLoading] = useState(false);
+    const [apiError, setApiError] = useState(false);    
 
     useEffect(() => {
         window.scrollTo(0, 0);
+        setShowLoading(true);
         fetch(`${config.API_ENDPOINT}/dialogue/by-chapter/${chapt}`)
             .then(res => {
                 if (res.ok) {
@@ -25,34 +28,41 @@ const Story = (props) => {
                 setStoryTitle(data.story_title)
                 const pages = data.pages.sort((a, b) => a.page - b.page);
                 setDialogue(pages);
+                setShowLoading(false);
+                setApiError(false);
             })
             .catch(error => {
+                setApiError(true);
+                setShowLoading(false);
+                window.scrollTo(0, document.querySelector('.Story__alert-div').offsetTop - document.querySelector('.Header__header').offsetHeight);
                 console.log('error', error);
             });
     }, [chapt]);
 
     return (
         <div className='Story__container'>
-            {dialogue.length 
-                ? 
-                    <>
-                        <h2 className='Story__h2'>{storyTitle}</h2>
-                        <DialoguePage 
-                            data={{
-                                response,
-                                page,
-                                setPage,
-                                setResponse,
-                                dialogue,
-                                chapt,
-                            }}
-                        />
-                    </>
-                :
-                    <>
-                        <p className='Story__loading'>Loading...</p>
-                    </>
+            {dialogue.length !== 0 &&
+                <>
+                    <h2 className='Story__h2'>{storyTitle}</h2>
+                    <DialoguePage 
+                        data={{
+                            response,
+                            page,
+                            setPage,
+                            setResponse,
+                            dialogue,
+                            chapt,
+                        }}
+                    />
+                </>
             }
+            {showLoading && <LoadingSpinner />}
+            <div
+                className='Story__alert-div'
+                role='alert'
+            >
+                {apiError && <p>Error: Looks like something went wrong. Check your connection and the url and try again.</p>}
+            </div>
         </div>
     );
 }
