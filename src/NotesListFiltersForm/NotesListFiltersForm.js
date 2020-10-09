65ecc6a1f1621ff11ca31c94d5d85b37cf250221
Luther Karper <luther.karper@live.com>
2020-10-08 21:08:46 -0400

    Add comments

--- src/NotesListFiltersForm/NotesListFiltersForm.js
+++ src/NotesListFiltersForm/NotesListFiltersForm.js
@@ -2,39 +2,48 @@ import React from 'react';
 import './NotesListFiltersForm.css';
 
 const NotesListFiltersForm = (props) => {
-
-    const { suffix, sortType, chapterFilter, setSortType, setChapterFilter } = props;
+    const { 
+        suffix, 
+        sortType, 
+        chapterFilter, 
+        setSortType, 
+        setChapterFilter, 
+    } = props;
 
     return (
-        <form className={`NotesListFiltersForm__form-${suffix}`}>
-            {suffix === 'widget-list' 
-                ?
-                    <fieldset>
-                        <legend>Filter by chapter</legend>
-                        <div>
-                            <input 
-                                type='radio'
-                                id='show-all-notes'
-                                name='filter-notes'
-                                checked={chapterFilter === 'all'}
-                                value='all'
-                                onChange={(e) => setChapterFilter(e.target.value)}
-                            />
-                            <label htmlFor='show-all-notes'>Show all notes</label> 
-                        </div>
-                        <div>
-                            <input 
-                                type='radio'
-                                id='show-chapt-notes'
-                                name='filter-notes'
-                                checked={chapterFilter === 'chapt'}
-                                value='chapt'
-                                onChange={(e) => setChapterFilter(e.target.value)}
-                            />
-                            <label htmlFor='show-chapt-notes'>Show notes from current chapter only</label> 
-                        </div>
-                    </fieldset>
-                : ''
+        <form 
+            className={`NotesListFiltersForm__form-${suffix}`}
+        >
+            {/* 
+                If suffix is set to 'widget-list' then the component is being used within a lesson.
+                If this is the case, then it is useful to be able to filter notes by chapter.    
+            */}
+            {suffix === 'widget-list' &&
+                <fieldset>
+                    <legend>Filter by chapter</legend>
+                    <div>
+                        <input 
+                            type='radio'
+                            id='show-all-notes'
+                            name='filter-notes'
+                            checked={chapterFilter === 'all'}
+                            value='all'
+                            onChange={(e) => setChapterFilter(e.target.value)}
+                        />
+                        <label htmlFor='show-all-notes'>Show all notes</label> 
+                    </div>
+                    <div>
+                        <input 
+                            type='radio'
+                            id='show-chapt-notes'
+                            name='filter-notes'
+                            checked={chapterFilter === 'chapt'}
+                            value='chapt'
+                            onChange={(e) => setChapterFilter(e.target.value)}
+                        />
+                        <label htmlFor='show-chapt-notes'>Show notes from current chapter only</label> 
+                    </div>
+                </fieldset>
             }
             <fieldset>
                 <legend>Sort by date saved</legend>
