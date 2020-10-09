2d518f26030ec4394da4d75d1c0d1166c83058f8
Luther Karper <luther.karper@live.com>
2020-10-08 20:16:54 -0400

    Update LearnHints component
    Refactored code to improve readability.

--- src/LearnHints/LearnHints.js
+++ src/LearnHints/LearnHints.js
@@ -3,27 +3,41 @@ import Hint from '../Hint/Hint';
 import './LearnHints.css';
 
 const LearnHints = (props) => {
-    const { page, showHintsBoolean, setShowHintsBoolean } = props;
+    const { 
+        page, 
+        showHintsBoolean, 
+        setShowHintsBoolean, 
+    } = props;
 
     const hideAndShowButton = showHintsBoolean 
-            ? <button className='LearnHints__hide-show-button button' onClick={() => setShowHintsBoolean(false)}>Hide hints</button>
-            : <button className='LearnHints__hide-show-button button' onClick={() => setShowHintsBoolean(true)}>Show hints</button>
-        ;
+            ? 
+                <button 
+                    className='LearnHints__hide-show-button button' 
+                    onClick={() => setShowHintsBoolean(false)}
+                >
+                    Hide hints
+                </button>
+            : 
+                <button 
+                    className='LearnHints__hide-show-button button' 
+                    onClick={() => setShowHintsBoolean(true)}
+                >
+                    Show hints
+                </button>;
     
     const hintsList = (
         <ol>
             {page.hints
                 .sort((a, b) => a.hint_order_number - b.hint_order_number)
-                .map(hint => (
-                    <Hint key={hint.hint_order_number} hint={hint} /> 
-                ))}
+                .map(hint => <Hint key={hint.hint_order_number} hint={hint} />)
+            }
         </ol>
     );
 
     return (
         <div className='LearnHints__container'>
             {hideAndShowButton}
-            {showHintsBoolean ? hintsList : ''}
+            {showHintsBoolean && hintsList}
         </div>
     );
 }
