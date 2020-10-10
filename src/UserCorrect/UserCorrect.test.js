e75f201ab7bb41a837eef236f8949a0c468e595f
Luther Karper <luther.karper@live.com>
2020-10-10 17:08:30 -0400

    Add tests for UserCorrect

---
+++ src/UserCorrect/UserCorrect.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import UserCorrect from './UserCorrect';
+
+describe('UserCorrect component', () => {
+    it('renders the UI without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<UserCorrect />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        const wrapper = mount(<UserCorrect />);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+        wrapper.unmount();
+    });
+});
