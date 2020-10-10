25da4cc4e1c711bf055caef268ce93cc5095dc8b
Luther Karper <luther.karper@live.com>
2020-10-10 13:36:37 -0400

    Add propTypes and defaultProps to DialogueResponse

--- src/DialogueResponse/DialogueResponse.js
+++ src/DialogueResponse/DialogueResponse.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './DialogueResponse.css';
 
 const DialogueResponse = (props) => {
@@ -6,19 +7,41 @@ const DialogueResponse = (props) => {
         dialogue, 
         choiceIndex, 
         page,
-    } = props.data;
-    
-    const { responses_to_choices } = dialogue[page - 1];
+    } = props;
 
-    const responsesArray = responses_to_choices.split("|");
-    
-    return (
-        <p 
-            className='DialogueResponse__text'
-        >
-            {responsesArray[choiceIndex]}
-        </p>
-    );
+    if (dialogue.length === 0) {
+        return (
+            <p 
+                className='DialogueResponse__text'
+            >
+                Error: Something went wrong. Check your connection and try again.
+            </p>
+        );
+    } else {
+        const { responses_to_choices } = dialogue[page - 1];
+
+        const responsesArray = responses_to_choices.split("|");
+        
+        return (
+            <p 
+                className='DialogueResponse__text'
+            >
+                {responsesArray[choiceIndex]}
+            </p>
+        );
+    }
 }
+
+DialogueResponse.defaultProps = { 
+    dialogue: [], 
+    choiceIndex: 0, 
+    page: 1,
+};
+
+DialogueResponse.propTypes = { 
+    dialogue: PropTypes.array, 
+    choiceIndex: PropTypes.number, 
+    page: PropTypes.number,
+};
 
 export default DialogueResponse;
