b4db6cfe6e0132f247053f0d2163bb0986a327ab
Luther Karper <luther.karper@live.com>
2020-10-10 16:23:57 -0400

    Add propTypes and defaultProps to QuestionLegend

--- src/QuestionLegend/QuestionLegend.js
+++ src/QuestionLegend/QuestionLegend.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const QuestionLegend = (props) => {
     const { 
@@ -6,7 +7,9 @@ const QuestionLegend = (props) => {
         savedUserInput, 
     } = props;
 
-    if (page.look_back) {
+    if (Object.keys(page).length === 0) {
+        return <p>Error: Looks like something went wrong. Check your connection and try again.</p>;
+    } else if (page.look_back) {
         const questionArray = page.question.split('|');
         const questionToDisplay = `${questionArray[0]}${savedUserInput[page.property_to_look_for]}${questionArray[1]}`;
         return <legend>{questionToDisplay}</legend>;
@@ -14,5 +17,15 @@ const QuestionLegend = (props) => {
 
     return <legend>{page.question}</legend>;
 }
+
+QuestionLegend.defaultProps = {
+    page: {},
+    savedUserInput: {},
+};
+
+QuestionLegend.propTypes = {
+    page: PropTypes.object,
+    savedUserInput: PropTypes.object,
+};
 
 export default QuestionLegend;
