2e78bb9b84af9af210665035102ea3ade1aadd8f
Luther Karper <luther.karper@live.com>
2020-10-12 11:21:55 -0400

    Add MainBackground component
    Added new component to use for background image.  This allows
    for styling that prevents white space from appearing on scroll in
    mobile.

---
+++ src/MainBackground/MainBackground.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainBackground.css';
+
+const MainBackground = (props) => {
+    return (
+        <div 
+            className='MainBackground__background-div'
+            role='img'
+            aria-hidden='true'
+        />
+    );
+}
+
+export default MainBackground;
