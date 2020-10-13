c92e93d4ae795a8ad1263a27f81c2be74fdd9fc8
Luther Karper <luther.karper@live.com>
2020-10-12 20:30:51 -0400

    Add comment

--- src/QuestionLegend/QuestionLegend.js
+++ src/QuestionLegend/QuestionLegend.js
@@ -10,6 +10,10 @@ const QuestionLegend = (props) => {
     if (Object.keys(page).length === 0) {
         return <p>Error: Looks like something went wrong. Check your connection and try again.</p>;
     } else if (page.look_back) {
+        /* 
+            If a question contains a 'look_back', then the app alters the question based on user input; 
+            the user's input is inserted into the question
+        */
         const questionArray = page.question.split('|');
         const questionToDisplay = `${questionArray[0]}${savedUserInput[page.property_to_look_for]}${questionArray[1]}`;
         return <legend>{questionToDisplay}</legend>;
